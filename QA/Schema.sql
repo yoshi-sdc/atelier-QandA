328d4590c1ba5309332c032fee983c26e0247ef6
ivykw <ivyw219@gmail.com>
2022-04-20 16:34:30 -0500

    Schema planned, database exists and tables are created. csv data loaded into tables.

---
+++ QA/Schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE questions(
+  id SERIAL NOT NULL PRIMARY KEY,
+  product_id INT NOT NULL,
+  question_body TEXT NOT NULL,
+  question_date BIGINT NOT NULL,
+  asker_name TEXT NOT NULL,
+  asker_email TEXT NOT NULL,
+  reported BOOLEAN NOT NULL,
+  question_helpfulness INT NOT NULL
+);
+CREATE TABLE answers(
+  id SERIAL NOT NULL PRIMARY KEY,
+  question_id INT references questions(id),
+  body TEXT NOT NULL,
+  date BIGINT NOT NULL,
+  answerer_name TEXT NOT NULL,
+  answerer_email TEXT NOT NULL,
+  reported BOOLEAN NOT NULL,
+  helpfulness INT NOT NULL
+);
+CREATE TABLE photos(
+  id SERIAL NOT NULL PRIMARY KEY,
+  answer_id INT references answers(id),
+  url TEXT NOT NULL
+);
+
+
