348fb841ada3d9aac47d09e0080a6ed6a0b6015f
ivykw <ivyw219@gmail.com>
2022-04-23 12:12:24 -0500

    refactored getting answers to use agg functions/json object building

--- database.js
+++ database.js
@@ -17,77 +17,88 @@ pool.connect((err, client, release) => {
   }
 });
 
+// SELECT q_id as question_id, question_body, question_date, asker_name, question_helpfulness, reported,
+//       () AS answers
 // function that takes a callback to send result to client
 async function getAll(product_id, page, count, callback) {
   try {
-    var finalResponse = {product_id: product_id}
     const skipped = (page - 1) * count;
-    var allQuestions =
-    `WITH q AS (
-      SELECT *
-      FROM questions q
-      WHERE product_id=${product_id}
-      ORDER BY q.q_id
-      LIMIT ${count}
-      OFFSET ${skipped})
-    SELECT q.question_body, q.question_date, q.asker_name, q.question_helpfulness,
-      a.question_id, a.body, a.date, a.answerer_name, a.helpfulness,
-      p.p_id, p.answer_id, p.url
-    FROM q
-    JOIN answers a
-    ON a.question_id=q.q_id
-    LEFT JOIN photos p
-    ON p.answer_id=a.a_id
-    WHERE q.reported=false AND a.reported=false
-    ORDER BY q.q_id`;
+    const allQuestions =
+    `SELECT a_id AS id, body, date, answerer_name, helpfulness,
+      (SELECT array_agg(url) FROM photos WHERE answer_id=a_id) AS photos
+    FROM answers a
+    WHERE a_id=5 AND a.reported=false`
     await pool.query(allQuestions)
-    .then ((res) => {
-      const data = res.rows;
-      var results = [];
-      var lastQ;
-      // iterate through data
-      for (let i = 0; i < data.length; i++) {
-        // if question id not already seen
-        const current = data[i];
-        if (!lastQ || lastQ['question_id'] !== current['question_id']) {
-          // make new object
-          var questionObj = {
-            question_id: current['question_id'],
-            question_body: current['question_body'],
-            question_date: current['question_date'],
-            asker_name: current['asker_name'],
-            question_helpfulness: current['question_helpfulness'],
-            reported: false,
-            answers: {}
-          }
-          lastQ = questionObj;
-          results.push(questionObj);
-        }
-        // if answer has not been seen before
-        if (!lastQ.answers['answer_id'])
-          // create new answer object
-          var answerObj = {
-            id: current['answer_id'],
-            body: current['body'],
-            date: current['date'],
-            answerer_name: current['answerer_name'],
-            answerer_email: current['answerer_email'],
-            helpfulness: current['helpfulness'],
-            photos: []
-          }
-          lastQ.answers[current['answer_id']] = answerObj;
-        // photos are always going to be unique, so if it exists, push it into answer's photos
-        if (current['p_id']) {
-          var photoObj = {
-            id: current['p_id'],
-            url: current['url']
-          }
-            lastQ.answers[current['answer_id']].photos.push(photoObj);
-        }
-      }
-      finalResponse['results'] = results;
-      callback(finalResponse);
+      .then((res) => {
+        callback(res.rows)
       })
+    const response = {product_id: product_id}
+    // var allQuestions =
+    // `WITH q AS (
+    //   SELECT *
+    //   FROM questions q
+    //   WHERE product_id=${product_id}
+    //   ORDER BY q.q_id
+    //   LIMIT ${count}
+    //   OFFSET ${skipped})
+    // SELECT q.question_body, to_timestamp(q.question_date), q.asker_name, q.question_helpfulness,
+    //   a.question_id, a.body, to_timestamp(a.date), a.answerer_name, a.helpfulness,
+    //   p.p_id, p.answer_id, p.url
+    // FROM q
+    // JOIN answers a
+    // ON a.question_id=q.q_id
+    // LEFT JOIN photos p
+    // ON p.answer_id=a.a_id
+    // WHERE q.reported=false AND a.reported=false
+    // ORDER BY q.q_id`;
+    // await pool.query(allQuestions)
+    // .then ((res) => {
+    //   const data = res.rows;
+    //   var results = [];
+    //   var lastQ;
+    //   // iterate through data
+    //   for (let i = 0; i < data.length; i++) {
+    //     // if question id not already seen
+    //     const current = data[i];
+    //     if (!lastQ || lastQ['question_id'] !== current['question_id']) {
+    //       // make new object
+    //       var questionObj = {
+    //         question_id: current['question_id'],
+    //         question_body: current['question_body'],
+    //         question_date: current['question_date'],
+    //         asker_name: current['asker_name'],
+    //         question_helpfulness: current['question_helpfulness'],
+    //         reported: false,
+    //         answers: {}
+    //       }
+    //       lastQ = questionObj;
+    //       results.push(questionObj);
+    //     }
+    //     // if answer has not been seen before
+    //     if (!lastQ.answers['answer_id'])
+    //       // create new answer object
+    //       var answerObj = {
+    //         id: current['answer_id'],
+    //         body: current['body'],
+    //         date: current['date'],
+    //         answerer_name: current['answerer_name'],
+    //         answerer_email: current['answerer_email'],
+    //         helpfulness: current['helpfulness'],
+    //         photos: []
+    //       }
+    //       lastQ.answers[current['answer_id']] = answerObj;
+    //     // photos are always going to be unique, so if it exists, push it into answer's photos
+    //     if (current['p_id']) {
+    //       var photoObj = {
+    //         id: current['p_id'],
+    //         url: current['url']
+    //       }
+    //         lastQ.answers[current['answer_id']].photos.push(photoObj);
+    //     }
+    //   }
+    //   finalResponse['results'] = results;
+    //   callback(finalResponse);
+    //   })
   } catch(err) {
     console.error('Error querying all questions', err)
   }
@@ -95,58 +106,26 @@ async function getAll(product_id, page, count, callback) {
 
 async function getAnswers(q_id, page, count, callback) {
   try {
-    var finalResponse = {
-      question: q_id,
-      page: page,
-      count: count,
-      results: []
-    }
     const skipped = (page - 1) * count;
-    // select answers where q_id = answer question_id (limited and offset)
-    // join with photos table on answer id = photos answer_id
-    var allAnswers =
-    `WITH a AS (
-      SELECT *
-      FROM answers a
-      WHERE question_id=${q_id} AND a.reported=false
-      ORDER BY a.a_id
-      LIMIT ${count}
-      OFFSET ${skipped})
-    SELECT a.body, a.date, a.answerer_name, a.helpfulness,
-      p.p_id, p.answer_id, p.url
-    FROM a
-    JOIN photos p
-    ON p.answer_id=a.a_id
-    ORDER BY a.a_id`;
+    const allAnswers =
+    `SELECT a_id AS answer_id, body, date, answerer_name, helpfulness,
+      (SELECT json_agg(json_build_object('id', p_id, 'url', url))
+      FROM photos WHERE answer_id=a_id) AS photos
+    FROM answers a
+    WHERE question_id=${q_id} AND reported=false
+    LIMIT ${count}
+    OFFSET ${skipped}`
     await pool.query(allAnswers)
-    .then ((res) => {
-      const data = res.rows;
-      var lastA;
-      // iterate overe data
-      for (let i = 0; i < data.length; i++) {
-        const current = data[i];
-        if (!lastA || lastA['answer_id'] !== current['answer_id']) {
-          var answerObj = {
-            answer_id: current['answer_id'],
-            body: current.body,
-            date: current.date,
-            answerer_name: current['answerer_name'],
-            helpfulness: current.helpfulness,
-            photos: []
-          }
-          lastA = answerObj;
-          finalResponse.results.push(answerObj);
-        }
-        if (current['p_id']) {
-          var photoObj = {
-            id: current['p_id'],
-            url: current.url
-          }
-          lastA.photos.push(photoObj);
-        }
-      }
-      callback(finalResponse);
-    })
+      .then((res) => {
+        var results = res.rows
+        const response = {
+          question: q_id,
+          page: page,
+          count: count,
+          results: results
+        };
+        callback(response)
+      })
   } catch (err) {
     console.error('Error querying answers', err)
   }
