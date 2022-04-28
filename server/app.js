26308a482b61f1110fc6af65e7dde7acb5318da9
ivykw <ivyw219@gmail.com>
2022-04-27 22:47:30 -0500

    trying to get loader.io to verify

--- server/app.js
+++ server/app.js
@@ -6,12 +6,9 @@ const port = process.env.SERVERPORT;
 var router = require('./routes.js');
 
 app.use(express.json());
-app.get ('ec2-54-177-194-184.us-west-1.compute.amazonaws.com:3000/loaderio-86f2e33a233b27c25850d8e19e495686.txt', (req, res) => {
-  res.sendFile('loaderio-86f2e33a233b27c25850d8e19e495686.txt', function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
+
+app.get(`/${process.env.LOADER}`, (req, res) => {
+  res.send(`${process.env.LOADER}`);
 });
 
 app.use('/qa', router);
